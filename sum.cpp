3715dc4622d90f1a18a545442367fabcf4535b60
Pravin Jedhe <pravinjedhe12@gmail.com>
2023-04-24 17:29:29 +0530

    new message added in sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -5,7 +5,7 @@ int main()
     int A = 10, B = 20, sum;
     
     sum = A + B;
-    cout << "Sum of two number " << A <<" and " << B << " = " << sum;
+    cout << "Sum of two number " << A <<" and " << B <<"is"<< sum;
     
     return 0;
 }
